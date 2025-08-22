932b8cd1db50c6a1ad288ba48e8e2333a4d4ebc5
Floris van Maldegem <fvmaldegem@gmail.com>
2025-08-22 16:09:45 +0200

    linter

--- app.py
+++ app.py
@@ -1,35 +1,43 @@
-import time
-import socket
-from flask import Flask, Request, request, render_template
-from werkzeug.datastructures import EnvironHeaders
+"""A simple python application to get node information"""
 
-hostname = None
-with open("/etc/hostname") as f:
-    hostname = f.read().strip(' \t\n\r')
+import time
+from flask import Flask, Request, request, render_template
+
+HOSTNAME = None
+
+with open("/etc/hostname", encoding="utf-8") as f:
+    HOSTNAME = f.read().strip(" \t\n\r")
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index() -> str:
-    return render_template('index.html.j2', server=get_server(), client=get_client(request))
+    """index route"""
+    return render_template(
+        "index.html.j2",
+        server=get_server(),
+        client=get_client(request)
+    )
 
 def get_server() -> dict:
+    """returns server information"""
     return {
-        'hostname': hostname,
-        'datetime': time.asctime(),
+        "hostname": HOSTNAME,
+        "datetime": time.asctime(),
     }
 
-def get_client(request: Request) -> dict:
-    real_ip = request.environ.get('HTTP_X_FORWARDED_FOR')
-    if real_ip is None: real_ip = request.environ.get('HTTP_X_REAL_IP')
-    if real_ip is None: real_ip = request.remote_addr
+def get_client(r: Request) -> dict:
+    """returns client information"""
+    real_ip = r.remote_addr
+    for header_name in ["HTTP_X_FORWARDED_FOR", "HTTP_X_REAL_IP"]:
+        value = r.headers.get(header_name)
+        if value is not None:
+            real_ip = value
 
     return {
-        'address': request.remote_addr,
-        'real_address': real_ip,
-        'method': request.method,
-        'uri': request.url,
-        'headers': request.headers
+        "address": r.remote_addr,
+        "real_address": real_ip,
+        "method": r.method,
+        "uri": r.url,
+        "headers": r.headers,
     }
-
-        
